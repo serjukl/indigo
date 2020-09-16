86489000cbab88892c63a128f2b9f68cd85ce5ce
serjukl <serjukl98@gmail.com>
2020-09-16 15:18:08 +0300

    nav is ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
+import { Provider } from 'react-redux'
+import { useStore } from '../store'
+
+import 'normalize.css'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const store = useStore(pageProps.initialReduxState)
+  return (
+      <Provider store={store}>
+          <Component {...pageProps} />
+      </Provider>
+  );
+};
+
 
 export default MyApp
