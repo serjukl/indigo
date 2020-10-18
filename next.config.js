8d735f3b61ff801f4f3942e884d0e8afda8ad129
serjukl <serjukl98@gmail.com>
2020-10-18 20:46:32 +0300

    fixed bugs

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withImages = require('next-images')
 module.exports = withImages()
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require('./scripts/siteMapCreator');
+        }
+        return config;
+    }
+};
