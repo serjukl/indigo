1f19a782c48c2c05f0d0ce519a83d86358506044
serjukl <serjukl98@gmail.com>
2020-11-25 11:07:36 +0200

    fixed bugs

--- pages/feedback.js
+++ pages/feedback.js
@@ -41,6 +41,11 @@ const Feedback = ({data}) => {
                 <meta name="keywords"
                       content="індіго експерт відгуки, залишити відгук про INDIGO EXPERT, INDIGO відгуки"/>
                 <meta name="google-site-verification" content="hLL3b-cRafAUAClrSSzfIPZ3ALPEBbDYHI6Xk7t9sVY" />
+                <meta property="og:title" content="Індіго Експерт - професійне знищення грибка" />
+                <meta property="og:site_name" content="Індіго Експерт - професійне знищення грибка" />
+                <meta property="og:url" content="https://indigo-expert.com.ua/" />
+                <meta property="og:description" content="Знищення та виведення грибка, методи виведення грибком з стін, м. Івано-Франківськ індіго експерт" />
+                <meta property="og:image" content="https://scontent.fiev25-1.fna.fbcdn.net/v/t1.0-1/c210.0.540.540a/44539065_1132148556940440_3142985560302288896_n.jpg?_nc_cat=105&ccb=2&_nc_sid=dbb9e7&_nc_ohc=QNGaPiyKMRIAX9EP4_c&_nc_ht=scontent.fiev25-1.fna&oh=b18bc64c7d580efdfaaa920e863c02f0&oe=5FE38489" />
             </Head>
             <Nav/>
             <div className={styles.feedMain}>
